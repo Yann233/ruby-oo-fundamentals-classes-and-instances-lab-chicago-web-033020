1d6d7d3019fc9f582db2268389a510d9675b9ec7
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-04-01 12:50:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,5 @@
-class Dog
-    def bark
-        puts "Woof!"
-    end
-###############
-    def sit
-        puts "The Dog is sitting"
-    end
+class Dog 
 end
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
